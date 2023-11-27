457536145c54bd5802fc5c09f2b3c45aa47fd564
Anaaaslagi <anas123hd@gmail.com>
2023-11-27 15:53:22 +0700

    Pertemuan 27 November 2023

--- resources/views/tambah.blade.php
+++ resources/views/tambah.blade.php
@@ -1,8 +1,6 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Tutorial Membuat CRUD Pada Laravel - www.malasngoding.com</title>
-</head>
+@extends('master2')
+
+@section('konten')
 <body>
 
 	<h2><a href="https://www.malasngoding.com">www.malasngoding.com</a></h2>
@@ -15,14 +13,16 @@
 
 	<form action="/pegawai/store" method="post">
 		{{ csrf_field() }}
-		Nama <input type="text" name="nama"> <br/>
+        <div class = "form-group">
+            <label for = "nama" class = "col-sm-2 control-label">Nama</label>
+            <div class = "col-sm-4">
+               <input type = "text" class = "form-control" id = "firstname" placeholder = "Masukan Nama Pegawai">
+            </div>
+         </div>
+
 		Jabatan <input type="text" name="jabatan"> <br/>
 		Umur <input type="number" name="umur"> <br/>
 		Alamat <textarea name="alamat"></textarea> <br/>
 		<input type="submit" value="Simpan Data">
 	</form>
-
-
-
-</body>
-</html>
+@endsection
